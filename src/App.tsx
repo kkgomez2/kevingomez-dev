c764bd08d6fd7de1e39625abe16f97373a7f9ed5
kkgomez2 <kkristoffergomez@gmail.com>
2025-04-22 11:55:22 -0700

    moving animation logic to component

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,10 @@
 import "./App.scss";
 import Header from "./components/Header";
-import { animate, createScope, Scope } from "animejs";
-import { useEffect, useRef } from "react";
 
 function App() {
-  const scope = useRef<Scope>(null);
-  const root = useRef(null);
-
-  useEffect(() => {
-    scope.current = createScope({ root }).add(() => {
-      // Every anime.js instances declared here are now scopped to <div ref={root}>
-
-      animate(".sentence span", {
-        y: [
-          { to: "-1.25rem", ease: "outExpo", duration: 600 },
-          { to: 0, ease: "outBounce", duration: 800, delay: 600 },
-        ],
-        // Property specific parameters
-        rotate: {
-          from: "-1turn",
-          delay: 500,
-        },
-        delay: (_, i) => i * 50 + 500, // Function based value
-        ease: "inOutCirc",
-        loopDelay: 2000,
-        loop: true,
-      });
-    });
-
-    // Properly cleanup all anime.js instances declared inside the scope
-    return () => {
-      scope!.current!.revert();
-    };
-  }, []);
 
   return (
-    <div ref={root}>
+    <div>
       <Header />
       <div className="body">
         <div className="bio-section">
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,40 @@
 import WordSpan from "./WordSpan"
+import { animate, createScope, Scope } from "animejs";
+import { useEffect, useRef } from "react";
 
 const Header = () => {
+  const scope = useRef<Scope>(null);
+  const root = useRef(null);
+
+    useEffect(() => {
+      scope.current = createScope({ root }).add(() => {
+        // Every anime.js instances declared here are now scopped to <div ref={root}>
+  
+        animate(".sentence span", {
+          y: [
+            { to: "-1.25rem", ease: "outExpo", duration: 600 },
+            { to: 0, ease: "outBounce", duration: 800, delay: 600 },
+          ],
+          // Property specific parameters
+          rotate: {
+            from: "-1turn",
+            delay: 500,
+          },
+          delay: (_, i) => i * 50 + 500, // Function based value
+          ease: "inOutCirc",
+          loopDelay: 2000,
+          loop: true,
+        });
+      });
+  
+      // Properly cleanup all anime.js instances declared inside the scope
+      return () => {
+        scope!.current!.revert();
+      };
+    }, []);
+
   return (
-    <div className="header">
+    <div className="header" ref={root}>
     <div className="logo">
       <div className="name">Kevin K Gomez</div>
       <div className="sentence">
