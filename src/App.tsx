9b902529ede4d63ad7e372fa06baa0b589136b28
kkgomez2 <kkristoffergomez@gmail.com>
2025-04-23 22:29:17 -0700

    Styling body

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,23 @@ import "./App.scss";
 import Header from "./components/Header";
 
 function App() {
-
   return (
-    <div>
+    <div className="main">
       <Header />
       <div className="body">
         <div className="bio-section">
-          Kevin Gomez is a full-stack web developer with 7+ years of
-          professional experience creating applications.
-          <br />
-          He has experience building applications with React, Node, .NET, AWS,
-          Azure, and tons more.
-          <br />
-          When he’s not doing that, he’s still going outside to play Pokémon GO.
+          <div>
+            Kevin Gomez is a <b>full-stack web developer</b> with 7+ years of
+            professional experience creating applications.
+          </div>
+          <div>
+            He has experience building applications with <b>React, Node, .NET, AWS,
+            Azure,</b> and tons more.
+          </div>
+          <div>
+            When he’s not doing that, he’s still going outside to play Pokémon
+            GO.
+          </div>
         </div>
       </div>
     </div>
