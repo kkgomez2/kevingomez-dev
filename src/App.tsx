e4c562a1ab9693b111dbe7bdb0be45a12ac74e40
kkgomez2 <kkristoffergomez@gmail.com>
2025-04-26 01:22:24 -0700

    Experience init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.scss";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Experience from "./components/Experience";
 
 function App() {
   return (
     <div className="main">
       <Header />
       <Hero />
+      <Experience />
     </div>
   );
 }
---
+++ src/components/Experience.tsx
@@ -0,0 +1,13 @@
+const Experience = () => {
+  return (
+    <div className="experience">
+      
+      <h1>Experience</h1>
+      <p>
+        I worked for companies in Chicago making solutions for salespeople. Now I live in LA and want to make solutions for other people.
+      </p>
+      </div>
+  )
+}
+
+export default Experience
