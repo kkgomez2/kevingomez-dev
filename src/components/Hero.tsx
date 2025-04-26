b8c3e76858184e049d88fe037d77c330e94ab0d1
kkgomez2 <kkristoffergomez@gmail.com>
2025-04-25 22:21:17 -0700

    Adding favicon, changing colors to css var, and adding hero greeting

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,33 +4,42 @@ const Hero = () => {
   return (
     <>
       <div className="hero">
-        <img className="hero-img" src="./me 001.jpg" />
-        <motion.div
-          initial={{ x: 4, y: 4 }}
-          animate={{
-            x: 0,
-            y: 0,
-            boxShadow: "10px 10px #ff0b55",
-            transition: {
-              delay: 0.2,
-              duration: 0.4,
-            },
-          }}
-          className="bio-section"
-        >
-          <p>
-            I'm a <b>full-stack web developer</b> with 7+ years of
-            professional experience creating applications.
-          </p>
-          <p>
-            I have experience building applications with{" "}
-            <b>React, Node, .NET, AWS, Azure,</b> and tons more.
-          </p>
-          <p>
-            When I'm not doing that, I'm still going outside to play Pokémon
-            GO.
-          </p>
-        </motion.div>
+        <div className="hero-img-container">
+          <img className="hero-img" src="./me 001.jpg" />
+        </div>
+        <div className="hero-text">
+        <div className="hero-greeting">
+            <h1>
+              <span>Hi, I'm Kevin.</span>
+            </h1>
+          </div>
+          <motion.div
+            initial={{ x: 4, y: 4 }}
+            animate={{
+              x: 0,
+              y: 0,
+              boxShadow: "10px 10px var(--red-main)",
+              transition: {
+                delay: 0.2,
+                duration: 0.4,
+              },
+            }}
+            className="bio-section"
+          >
+            <p>
+              I'm a <b>full-stack web developer</b> with 7+ years of
+              professional experience creating applications.
+            </p>
+            <p>
+              I have experience building applications with{" "}
+              <b>React, Node, .NET, AWS, Azure,</b> and tons more.
+            </p>
+            <p>
+              When I'm not doing that, I'm still going outside to play Pokémon
+              GO.
+            </p>
+          </motion.div>
+        </div>
       </div>
     </>
   );
