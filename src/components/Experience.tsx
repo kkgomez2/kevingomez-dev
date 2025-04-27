0e9b56605ae083d8661812e39c69f6d196a1cd64
kkgomez2 <kkristoffergomez@gmail.com>
2025-04-27 13:27:57 -0700

    Adding basic experience section

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,4 +1,5 @@
 import resume from "../data/resume";
+import InViewAppear from "./utilities/InViewAppear";
 
 const Experience = () => {
   const entries = resume.entries;
@@ -6,33 +7,55 @@ const Experience = () => {
   return (
     <div className="container-red">
       <div className="experience">
-        <h1><span>Experience</span></h1>
+        <InViewAppear color="--white">
+          <h1>
+            <span>Experience</span>
+          </h1>
+        </InViewAppear>
 
         {entries.map(({ company, location, experience }, i) => {
           return (
             <>
-              <div key={i}>
-                <h2>
-                  {company} - {location}
-                </h2>
+              <div className="experience-employer-section" key={i}>
+                <InViewAppear color="--white">
+                  <div>
+                    <b>{company}</b>
+                  </div>
+                </InViewAppear>
+
+                <InViewAppear color="--white">
+                  <div>{location}</div>
+                </InViewAppear>
               </div>
               <div>
                 {experience.map(({ title, dateRange, summary, points }, j) => {
                   return (
-                    <>
-                      <div key={j}>
+                    <div className="experience-position" key={j}>
+                      <div className="experience-position-title">
                         <div>
-                          <h3>{title}</h3>
+                          <InViewAppear color="--white">
+                            <h3>{title}</h3>
+                          </InViewAppear>
                         </div>
-                        <div>{dateRange}</div>
+
+                        <InViewAppear color="--white">
+                          <div>{dateRange}</div>
+                        </InViewAppear>
                       </div>
                       <div>
-                        {summary}
-                        {/* {points.map((point, k) => {
-                          return <div key={k}>- {point}</div>;
-                        })} */}
+                        {/* {summary} */}
+                        {points.map((point, k) => {
+                          return (
+                            <InViewAppear color="--white">
+                              <div key={k}>
+                                <span style={{ marginRight: "10px" }}>●</span>
+                                {point}
+                              </div>
+                            </InViewAppear>
+                          );
+                        })}
                       </div>
-                    </>
+                    </div>
                   );
                 })}
               </div>
