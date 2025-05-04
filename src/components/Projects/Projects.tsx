64c20542453c1bd6277e7c77672cf6ce230a4ab4
kkgomez2 <kkristoffergomez@gmail.com>
2025-05-03 20:45:50 -0700

    Functioning swiper

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,5 +1,11 @@
 import "./Projects.scss";
+import "../../../node_modules/swiper/swiper.scss";
+import "../../../node_modules/swiper/modules/navigation.scss";
+import "../../../node_modules/swiper/modules/pagination.scss";
 import projects from "../../data/projects";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
 
 const Projects = () => {
   const entries = projects.entries;
@@ -9,45 +15,56 @@ const Projects = () => {
       <div className="projects">
         <h1>Projects</h1>
 
-        <div className="projects-carousel">
+        <Swiper
+          navigation={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Navigation, Pagination]}
+          className="projects-carousel"
+        >
           {" "}
           {entries.map(({ title, link, image, summary, technologies }, i) => {
             if (title === "N/A") {
               return (
-                <div key={i} className="projects-item-more">
-                  And more to come!
-                </div>
+                <SwiperSlide>
+                  <div key={i} className="projects-item projects-item-more">
+                    And more to come!
+                  </div>
+                </SwiperSlide>
               );
             } else {
               return (
-                <div key={i} className="projects-item">
-                  <a
-                    href={link}
-                    target="_blank"
-                    className="projects-item-title"
-                  >
-                    <img
-                      style={{ maxWidth: "500px" }}
-                      src={image}
-                      alt={title}
-                    />
-                  </a>
-                  <h2>{title}</h2>
-                  <p className="projects-item-summary">{summary}</p>
-                  <div className="technologies">
-                    {technologies.map((tech, j) => {
-                      return (
-                        <span className="chip" key={j}>
-                          {tech}
-                        </span>
-                      );
-                    })}
+                <SwiperSlide>
+                  <div key={i} className="projects-item">
+                    <a
+                      href={link}
+                      target="_blank"
+                      className="projects-item-title"
+                    >
+                      <img
+                        style={{ maxWidth: "500px" }}
+                        src={image}
+                        alt={title}
+                      />
+                    </a>
+                    <h2>{title}</h2>
+                    <p className="projects-item-summary">{summary}</p>
+                    <div className="technologies">
+                      {technologies.map((tech, j) => {
+                        return (
+                          <span className="chip" key={j}>
+                            {tech}
+                          </span>
+                        );
+                      })}
+                    </div>
                   </div>
-                </div>
+                </SwiperSlide>
               );
             }
           })}
-        </div>
+        </Swiper>
       </div>
     </div>
   );
