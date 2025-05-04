af6b2e76d7882749ef8297924dc78c92c616673f
kkgomez2 <kkristoffergomez@gmail.com>
2025-05-03 20:15:47 -0700

    displaying projects

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,10 +1,53 @@
 import "./Projects.scss";
+import projects from "../../data/projects";
 
 const Projects = () => {
+  const entries = projects.entries;
+
   return (
     <div className="projects-container">
       <div className="projects">
         <h1>Projects</h1>
+
+        <div className="projects-carousel">
+          {" "}
+          {entries.map(({ title, link, image, summary, technologies }, i) => {
+            if (title === "N/A") {
+              return (
+                <div key={i} className="projects-item-more">
+                  And more to come!
+                </div>
+              );
+            } else {
+              return (
+                <div key={i} className="projects-item">
+                  <a
+                    href={link}
+                    target="_blank"
+                    className="projects-item-title"
+                  >
+                    <img
+                      style={{ maxWidth: "500px" }}
+                      src={image}
+                      alt={title}
+                    />
+                  </a>
+                  <h2>{title}</h2>
+                  <p className="projects-item-summary">{summary}</p>
+                  <div className="technologies">
+                    {technologies.map((tech, j) => {
+                      return (
+                        <span className="chip" key={j}>
+                          {tech}
+                        </span>
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+            }
+          })}
+        </div>
       </div>
     </div>
   );
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -6,15 +6,15 @@ const projects = {
       image: "/projects/movies.png",
       dateRange: "Apr 2025",
       summary: "Simple site to query The Movie Database API, search for the most relevant movies, and also save searches to AppWrite database to serve as 'trending' movies. You have the power to change what's trending (literally! Just search your favorite movie a few times).",
-      tech: ["React.js", "AppWrite", "Vite", "Vercel",],
+      technologies: ["React.js", "AppWrite", "Vite", "Vercel",],
     },
     {
       title: "Acting Portfolio Site",
       link: "https://kevinkgomez.com",
       image: "/projects/acting.png",
       dateRange: "Jan 2016 - Present",
-      summary: "Acting portfolio site for my voice acting work. Written in Next.js for server-side rendering and SEO. Responsive, with simple three column design to differentiate from other, single column full width sites. But if I could refactor it, I would DEFINITELY re-style it with grid instead of flex.",
-      tech: ["React.js", "Next.js", "Tailwind", "Vercel",],
+      summary: "Acting portfolio site for my voice acting work. Written in Next.js to utilize server-side rendering's SEO benefit. Responsive, with simple three column design to differentiate from other, single column full width sites. But if I could refactor it, I would DEFINITELY re-style it with grid instead of flex.",
+      technologies: ["React.js", "Next.js", "Tailwind", "Vercel",],
     },
     {
       title: "Developer Portfolio Site (this site!)",
@@ -22,7 +22,7 @@ const projects = {
       image: "/projects/here.png",
       dateRange: "Apr 2025 - Present",
       summary: "Developer resume and portfolio site, written to introduce you to me and my experience. I hadn't had much chance to use Anime.js or Framer Motion (just Motion now I believe) in a professional capacity, so it was nice actually learn how to utilize them here (and beyond!).",
-      tech: ["React.js", "TypeScript", "Anime.js", "Framer Motion", "Vite", "Vercel"],
+      technologies: ["React.js", "TypeScript", "Anime.js", "Framer Motion", "Vite", "Vercel"],
     },
     {
       title: "N/A",
@@ -30,7 +30,7 @@ const projects = {
       image: "",
       dateRange: "",
       summary: "",
-      tech: [""],
+      technologies: [""],
     },
   ],
 };
