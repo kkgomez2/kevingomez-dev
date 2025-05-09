607f3372a711ffc2dd4a4e552d812e8eb4b93ddf
kkgomez2 <kkristoffergomez@gmail.com>
2025-05-08 17:07:46 -0700

    Displaying basic nav

---
+++ src/components/sections/Nav/Nav.tsx
@@ -0,0 +1,16 @@
+import './Nav.scss'
+
+const Nav = () => {
+  return (
+    <div className="nav-container">
+      <nav className="nav">
+        <div className="nav-item">About</div>
+        <div className="nav-item">Experience</div>
+        <div className="nav-item">Projects</div>
+        <div className="nav-item">Contact</div>
+      </nav>
+    </div>
+  );
+};
+
+export default Nav;
